e24b0ea930a143177d60eec3cb1c67673b6fe0db
Alexis HELSON <email@example.com>
2019-07-29 12:47:39 +0200

    [+]

--- libft_genesis/includes/ft_file.h
+++ libft_genesis/includes/ft_file.h
@@ -6,7 +6,7 @@
 /*   By: alhelson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 15:57:55 by alhelson          #+#    #+#             */
-/*   Updated: 2019/07/27 15:22:57 by alhelson         ###   ########.fr       */
+/*   Updated: 2019/07/29 12:47:27 by alhelson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_file_exist(char *name);
 
 int		ft_file_size(char *name);
 
-size_t		ft_file_size2(char *name);
+size_t	ft_file_size2(char *name);
 
 char	*ft_file_return_data(char *name);
 
