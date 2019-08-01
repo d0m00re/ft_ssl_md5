e60562270230e0fb815c2656fe71d16035333862
Alexis HELSON <email@example.com>
2019-08-01 17:20:12 +0200

    [+] yhea update fd0

--- srcs/ft_arg/ft_manage_arg_p.c
+++ srcs/ft_arg/ft_manage_arg_p.c
@@ -13,33 +13,27 @@
 #include "ft_display.h"
 #include "ft_arg.h"
 #include <stdlib.h>
-
-static char	*get_fd0(void)
-{
-	char	*buffer;
-	int		count;
-
-	count = 0;
-	if (!(buffer = malloc(sizeof(char) * 10000)))
-		return (0);
-	ft_bzero(buffer, 10000);
-	while (read(0, &(buffer[count]), 1) && buffer[count] != 10)
-		count++;
-	return (buffer);
-}
+#include "string_resize.h"
 
 void		ft_manage_arg_p(t_arg arg, char *(hash)(char *str, size_t size))
 {
 	char	*tstr;
 	char	*buff;
+	size_t size;
 
 	tstr = 0;
+	size = 0;
+	buff = 0;
 	if (arg.p)
 	{
-		buff = get_fd0();
+		if ((get_fd0(&buff, &size)))
+		{
+			ft_putstr("error ... fd0\n");
+			return ;
+		}
 		if (arg.pp)
 			ft_putstr(buff);
-		tstr = hash(buff, ft_strlen(buff));
+		tstr = hash(buff, size);
 		ft_putstr(tstr);
 		ft_putchar('\n');
 		free(tstr);
